c1645066edc0d3dfa8b23c2de7aa2def2eee8d57
Nir1218-svg <nirablof@gmail.com>
2026-06-19 12:57:11 +0600

    imported temp

--- email.py
+++ email.py
@@ -1,6 +1,13 @@
-
+import time
+import requests
+import schedule
+import smtplib
 from email.message import EmailMessage
-sender="nirabbarua362@gmail.com"
-app_pass="qswg ecoc kbus lcij"
-reciver="nirablof@gmail.com"
-msg=EmailMessage()
+def scrape_temp():
+    api_key="cur_live_h7laJNRxHYGkFcR4mnYEChqzOV6KG9QKMHfMpB03"
+    api_url=f"https://api.currencyapi.com/v3/latest?apikey={api_key}&base_currency=USD&currencies=BDT"
+    response=requests.get(api_url)
+    data=response.json()
+    print(response.status_code)
+    bdt=data["data"]["BDT"]["value"]
+    return bdt
